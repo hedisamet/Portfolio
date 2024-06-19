0ce09b157deadd91b186f5949b410b9a46261ba7
hedisamet <hedisamet20@gmail.com>
2024-06-20 00:55:32 +0100

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Hedi Samet</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#" className="footer__link">
+              Project
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.linkedin.com"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="bx bxl-facebook"></i>
+          </a>
+          <a
+            href="https://github.com"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="bx bxl-instagram"></i>
+          </a>
+          <a
+            href="https://twitter.com"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="bx bxl-twitter"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">
+        &#169; All rigths reserved
+        </span>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
